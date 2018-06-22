e5acca29707e295aae7cff52b81894fc5c57395e
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 09:16:30 +0700

    Update ConvertBitmap.java

--- src/com/sentosatech/worldcup2014de/utility/ConvertBitmap.java
+++ src/com/sentosatech/worldcup2014de/utility/ConvertBitmap.java
@@ -1,3 +1,10 @@
+/*
+ * Name: ConvertBitmap.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem, Inc. All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.utility;
 
 import java.io.IOException;
