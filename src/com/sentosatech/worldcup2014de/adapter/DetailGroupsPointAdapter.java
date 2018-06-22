464fc9e12f7ce6dabcbe096b55068e3ef77ba748
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 09:52:38 +0700

    Update DetailGroupsPointAdapter.java

--- src/com/sentosatech/worldcup2014de/adapter/DetailGroupsPointAdapter.java
+++ src/com/sentosatech/worldcup2014de/adapter/DetailGroupsPointAdapter.java
@@ -1,3 +1,10 @@
+/*
+ * Name: DetailGroupsPointAdapter.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.adapter;
 
 import java.util.ArrayList;
