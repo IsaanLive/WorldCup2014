0a23fd16721cb0489a2bafe959de9e999e01bb80
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 09:51:03 +0700

    Update WebServiceConfig.java

--- src/com/sentosatech/worldcup2014de/activity/config/WebServiceConfig.java
+++ src/com/sentosatech/worldcup2014de/activity/config/WebServiceConfig.java
@@ -1,10 +1,12 @@
+/*
+ * Name: WebServiceConfig.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.activity.config;
 
-/**
- * WebServiceConfig class contains web service settings
- * 
- * @author Lemon
- */
 public final class WebServiceConfig {
 	// Network time out: 60s
 	public static int NETWORK_TIME_OUT = 100000;
@@ -23,13 +25,13 @@ public final class WebServiceConfig {
 	public static String PROTOCOL_HTTP = "http://";
 	public static String PROTOCOL_HTTPS = "https:";
 //	 public static String APP_DOMAIN = PROTOCOL_HTTP
-//	 + "fruitysolution.vn:8888/wc2014/backend/api/";
+//	 + "euroasia.company:8888/wc2014/backend/api/";
 
 	public static String APP_DOMAIN = PROTOCOL_HTTP
-			+ "worldcup2014.guiltyplanet.com/backend/api/";
+			+ "wc2014.euroasia.compnay/backend/api/";
 
 	// ===================== WEB SERVICE LINK =====================
-//	public static String URL_CHECK_VERSION = "http://fruitysolution.vn:8888/wc2014/version/han_version.txt";
+//	public static String URL_CHECK_VERSION = "https://euroasia.company:8888/wc2014/version/version.txt";
 
 	public static String URL_GET_ALL_SCHEDULE = APP_DOMAIN + "schedule?t=1";
 	public static String URL_GET_GROUP_DETAIL = APP_DOMAIN + "group?g=";
@@ -39,7 +41,7 @@ public final class WebServiceConfig {
 	public static String URL_GET_TREE_MAP = APP_DOMAIN + "sitemap";
 	public static String URL_GET_TOP_SCORE = APP_DOMAIN + "topscore";
 	public static String URL_REGISTER_VERSION = APP_DOMAIN + "versionText?vs=";
-//	public static String URL_REGISTER_DEVICE="http://jayadata.co.id/bola/api/store_device_id";
+//	public static String URL_REGISTER_DEVICE="http://euroasia.compamny/wc2014/api/store_device_id";
 
 	// ===================== PARAMETER =====================
 
