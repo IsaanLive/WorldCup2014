7cd843581faa03349cb0e663992d4ceee4e02240
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 09:28:32 +0700

    Update MemoryCache.java

--- src/com/sentosatech/worldcup2014de/utility/lazylist/MemoryCache.java
+++ src/com/sentosatech/worldcup2014de/utility/lazylist/MemoryCache.java
@@ -1,3 +1,10 @@
+/*
+ * Name: MemoryCache.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.utility.lazylist;
 
 import java.lang.ref.SoftReference;
