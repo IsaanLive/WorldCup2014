5cbc48d55dda61fc81ce56712cb39b50de1e7db2
Wolfgang Holzem <shah@ehalal.io>
2018-06-22 10:44:47 +0700

    Update IcsLinearLayout.java

--- src/com/sentosatech/worldcup2014de/indicator/IcsLinearLayout.java
+++ src/com/sentosatech/worldcup2014de/indicator/IcsLinearLayout.java
@@ -1,3 +1,10 @@
+/*
+ * Name: IcsLinearLayout.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.indicator;
 
 import android.content.Context;
