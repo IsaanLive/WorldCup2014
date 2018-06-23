a22d7aae425fdd9c169f6817eed73f944df7499e
Wolfgang Holzem <wolfgang.sladkowski@gmail.com>
2018-06-23 14:32:40 +0700

    Update DatabaseConfig.java

--- src/com/sentosatech/worldcup2014de/info/DatabaseConfig.java
+++ src/com/sentosatech/worldcup2014de/info/DatabaseConfig.java
@@ -1,3 +1,10 @@
+/*
+ * Name: DatabaseConfig.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.info;
 
 import com.sentosatech.worldcup2014de.PacketUtility;
