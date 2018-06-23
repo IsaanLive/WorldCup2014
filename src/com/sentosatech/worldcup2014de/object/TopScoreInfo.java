866a701a4865a2231bec986c57896e2846f1dd85
Wolfgang Holzem <shah@ehalal.io>
2018-06-23 15:57:55 +0700

    Update TopScoreInfo.java

--- src/com/sentosatech/worldcup2014de/object/TopScoreInfo.java
+++ src/com/sentosatech/worldcup2014de/object/TopScoreInfo.java
@@ -1,3 +1,10 @@
+/*
+ * Name: TopScoreInfo.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.object;
 
 public class TopScoreInfo {
