82472cc80646a128aed6face52087c06e434df04
Wolfgang Holzem <shah@ehalal.io>
2018-06-23 15:47:22 +0700

    Update AsyncHttpGet.java

--- src/com/sentosatech/worldcup2014de/network/AsyncHttpGet.java
+++ src/com/sentosatech/worldcup2014de/network/AsyncHttpGet.java
@@ -1,3 +1,10 @@
+/*
+ * Name: AsyncHttpGet.java,v $
+ * Version: $Revision: 1.4 $
+ * Date: $Date: Nov 15, 2013 2:05:59 PM $
+ *
+ * Copyright (C) Wolfgang Holzem - All rights reserved.
+ */
 package com.sentosatech.worldcup2014de.network;
 
 import java.net.URLEncoder;
